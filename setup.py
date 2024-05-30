f4b5848e821a37d217e9a5871dd45f4e4c12a354
athuler <22741115+athuler@users.noreply.github.com>
2024-05-29 21:14:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
 	name='Python Packaging Test',
-	version='0.1.3',
+	version='0.1.4',
 	description='A test of the packaging',
 	long_description=long_description,
 	author='Andrei Thuler',
